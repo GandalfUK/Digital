0722e359aa4f0d658b3af60f5644a435a19e9335
GandalfUK <GitHub@ShelobsLair.net>
2018-07-18 16:30:48 +0100

    Added Konrad Zuse’s Full Adder Circuit as a test case for the multi-pole double-throw switch. Invented for the Z3 computer built in 1941.

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -45,8 +45,8 @@ public class TestExamples extends TestCase {
     public void testTestExamples() throws Exception {
         File examples = new File(Resources.getRoot(), "/dig/test");
         testCasesInFiles = 0;
-        assertEquals(137, new FileScanner(TestExamples::check).scan(examples));
-        assertEquals(129, testCasesInFiles);
+        assertEquals(138, new FileScanner(TestExamples::check).scan(examples));
+        assertEquals(130, testCasesInFiles);
     }
 
     /**
